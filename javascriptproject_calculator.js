c6e526f00397bb6ae4cc1d72759a794b61060eb8
MeshBright <mesh.eovoa@gmail.com>
2025-08-07 02:21:29 +0100

    Add files via upload
    This is a basic calculator written using Javascript. It takes 3 numbers and 2 operands (addition, subtraction, multiplication, and division). The project helped work on fundamental concepts like your common conditional statements (if-else and switch statements)

---
+++ javascriptproject_calculator.js
@@ -0,0 +1,141 @@
+/*
+CODE THAT HELPS YOU TAKE IN INPUT FROM THE VSCODE TERMINAL(WHICH SEEMS LESS COMMON WHILE USING JAVASCRIPT)
+const prompt = require('prompt-sync')();
+let username = prompt("What is your name? ");
+console.log(`Hello, ${username}`);
+
+*/
+
+const prompt = require('prompt-sync')();
+let number = [];
+let operands = [];
+let solution;
+let j = 0;
+
+for (let i = 0; i < 3; ++i ) {
+    number[i] = Number(prompt("Input a number: "));
+
+    if (j === 2) {
+        continue;
+    }
+    else {
+        operands[j] = prompt("Input an operation: "); 
+        ++j;
+    }
+    
+}
+
+let i = 0;
+j = i + 1;
+let k = j + 1;
+
+    if (operands[i] === "+") {
+         
+        switch (operands[j]) {
+            case "+":
+            solution = number[i] + number[j] + number[k];
+            break;
+
+            case "-":
+            solution = (number[i] + number[j]) - number[k];
+            break;
+
+            case "/":
+            if (number[k] === 0) {
+                console.log("Please input a value other than zero!");
+                break;
+            }
+            else {
+                solution = number[i] + (number[j] / number[k]);
+                break;
+            }
+            case "*":
+            solution = number[i] + (number[j] * number[k]);
+            break;
+        }
+    }
+
+    else if (operands[i] === "-") {
+
+        switch (operands[j]) {
+            case "+":
+            solution = number[i] - (number[j] + number[k]);
+            break;
+
+            case "-":
+            solution = number[i] - (number[j] - number[k]);
+            break;
+            
+            case "/":
+            if (number[k] === 0) {
+                console.log("Please input a value other than zero!");
+                break;
+            }
+            else {
+                solution = number[i] - (number[j] / number[k]);
+                break;
+            }
+            
+            case "*":
+            solution = number[i] - (number[j] * number[k]);
+            break;
+        }
+    }
+
+    else if (operands[i] === "/") {
+        if (number[j] === 0) {
+            console.log("Please input a value other than zero!");
+        }
+        else {
+            switch (operands[j]) {
+                case "+":
+                solution = (number[i] / number[j]) + number[k];
+                break;
+
+                case "-":
+                solution = (number[i] / number[j]) - number[k];
+                break;
+
+                case "/":
+                if (number[k] === 0) {
+                    console.log("Please input a value other than zero!");
+                    break;
+                }
+                else {
+                    solution = number[i] / (number[j] / number[k]);
+                    break;
+                }
+                case "*":
+                    solution = (number[i] / number[j]) * number[k];
+                    break;
+                }
+        }
+    }
+   
+    else if (operands[i] === "*") {
+        switch (operands[j]) {
+            case "+":
+            solution = (number[i] * number[j]) + number[k];
+            break;
+
+            case "-":
+            solution = (number[i] * number[j]) - number[k];
+            break;
+
+            case "/":
+            if (number[k] === 0) {
+                console.log("Please input a value other than zero!");
+                break;
+            }
+            else {
+                solution = number[i] * (number[j] / number[k]);
+                break;
+            }
+            case "*":
+                solution = number[i] * (number[j] * number[k]);
+                break;
+            }
+    }
+    
+
+console.log("The solution is: ", solution);
